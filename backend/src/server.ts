458ab03175bb94b2ba9629c49e4d520b915269fc
mumimud <narephatrung@gmail.com>
2026-03-28 18:04:42 +0700

    fix: corsOptions

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,10 +14,9 @@ async function bootstrap() {
     const corsOptions = {
       origin: "https://my-tripmate-wepapp-1.onrender.com",
       credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
     };
-
+    //methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    //allowedHeaders: ["Content-Type", "Authorization"],
     //app.options("/*", cors(corsOptions));
     app.use(cors(corsOptions));
 
